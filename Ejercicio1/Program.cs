7bb5435dbfb52d4a204ec18828062292554e641b
LauBena <87872842+LauBena@users.noreply.github.com>
2022-07-08 10:17:31 -0300

    Cambios realizados, ejercicio1 resuelto

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ejercicio 1
+//Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Tu nombre es: " + nombre);
+
+//Ejercicio 2
+//Escribe un programa que tome la hora y la escriba por pantalla.
+
+Console.WriteLine("Escribe la hora: ");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora es: " + hora);
